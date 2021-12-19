aec00c9e15a619d6a48d58ee40b4f6aff9b5ebea
Altan013 <altanai@code.edu.az>
2021-12-19 22:19:06 +0400

    ev tapshirigi

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp3.Extension;
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world".Find("wo"));
+        }
+    }
+}
